c6f166d77a825c22a5ab83f7872dc27dd2b034e0
Aisha <arsaevaaisha@gmail.com>
2023-02-28 01:57:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openingBrackets = bracketsConfig.map(pair => pair[0]);
+  const closingBrackets = bracketsConfig.map(pair => pair[1]);
+  
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+    
+    if (openingBrackets.includes(currentChar)) {
+      // Push opening brackets to stack
+      stack.push(currentChar);
+    } else if (closingBrackets.includes(currentChar)) {
+      // Check if closing bracket matches the last opening bracket in the stack
+      const matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(currentChar)];
+      if (stack.length === 0 || stack.pop() !== matchingOpeningBracket) {
+        return false;
+      }
+    } else {
+      // Ignore non-bracket characters
+      continue;
+    }
+  }
+  
+  // If there are still opening brackets in the stack, the string is unbalanced
+  return stack.length === 0;
 }
